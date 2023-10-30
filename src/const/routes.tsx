3b4a6fb328124f0564858dc3e37936bb1715fa4a
Samuel Burke <samuel.burke@emplifi.io>
2023-10-30 22:22:08 +0100

    create routes, update eslint config, wrap app in mantine provider

---
+++ src/const/routes.tsx
@@ -0,0 +1,14 @@
+export const ROUTES = [
+  {
+    path: '/',
+    element: <div>login route</div>,
+  },
+  {
+    path: '/register',
+    element: <div>register route</div>,
+  },
+  {
+    path: '/home',
+    element: <div>Home</div>,
+  },
+];
