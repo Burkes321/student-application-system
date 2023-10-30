bc22486548f4a6a69ef501a0df379587663808d4
Samuel Burke <samuel.burke@emplifi.io>
2023-10-30 22:22:08 +0100

    create routes, update eslint config, wrap app in mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './main.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+
+import { ROUTES } from './const/routes.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const router = createBrowserRouter(ROUTES);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </React.StrictMode>
+);
