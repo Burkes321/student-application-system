32c626c6300cf29b763d147a893adeaea87a816c
Samuel Burke <samuel.burke@emplifi.io>
2023-10-31 01:22:47 +0100

    removed react svg, created fake user, email validating function, handle invalid email

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -7,37 +7,60 @@ import {
   PasswordInput,
   TextInput,
 } from '@mantine/core';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import css from './login.module.css';
 
 import { Page } from '../../components/page/Page';
 
 export const Login = () => {
-  // could implement useReducer hook here but since we only have two fields, use state is fine
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const fakeUser = {
+    email: 'fakeUser@fake.com',
+    password: 'password1',
+  };
 
-  const handleFormSubmit = useCallback(() => {}, []);
+  // could implement useReducer hook here but since we only have two fields, use state is fine
+  const [email, setEmail] = useState('test@test.com');
+  const [password, setPassword] = useState('passwordhere');
+  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
+
+  // mantine has build in form validation but it made sense for this task to do the email by hand
+  const validateEmail = useCallback(
+    (email: string) => /^\S+@\S+$/.test(email),
+    []
+  );
+
+  const handleFormSubmit = useCallback(() => {
+    if (!validateEmail) {
+      setIsEmailInvalid(true);
+      return;
+    }
+
+    setIsEmailInvalid(false);
+
+    // TODO: check here against some dummy user and if the credentials then redirect to the home page
+  }, [validateEmail]);
 
   return (
     <Page>
       <div className={css.formContainer}>
         <Paper className={css.form} withBorder shadow="md" p={30} radius="md">
-          <TextInput label="Email" placeholder="test@test.com" required />
+          <div>Please enter your email and password</div>
+          <TextInput
+            label="Email"
+            placeholder="test@test.com"
+            required
+            mt={10}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
           <PasswordInput
             label="Password"
             placeholder="Your password"
             required
             mt="md"
+            onChange={(event) => setPassword(event.currentTarget.value)}
           />
-          <Group justify="space-between" mt="lg">
-            <Checkbox label="Remember me" />
-            <Anchor component="button" size="sm">
-              Forgot password?
-            </Anchor>
-          </Group>
-          <Button fullWidth mt="xl">
+          <Button fullWidth mt="xl" onClick={handleFormSubmit}>
             Sign in
           </Button>
         </Paper>
