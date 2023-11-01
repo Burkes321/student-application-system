1dbaa3be0ed472614d973e4bf914719ddc950f42
Samuel Burke <samuel.burke@emplifi.io>
2023-11-02 00:44:10 +0100

    create array of fake users

--- src/routes/home/const.ts
+++ src/routes/home/const.ts
@@ -1,1 +1,54 @@
+import { faker } from '@faker-js/faker';
 
+const findRandomEntry = (max: number, min: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const UNIVERSITIES = [
+  'University of Cambridge',
+  'University of Oxford',
+  'London School of Economics',
+  'University of St Andrews',
+  'University of Bath',
+  'Imperial College London',
+  'Loughborough University',
+  'Durham Univeristy',
+  'UCL',
+  'Lancaster University',
+];
+
+const DURATIONS = [
+  '1 year',
+  '2 years',
+  '3 years',
+  '4 years',
+  '5 years',
+  '6 years',
+  '7 years',
+  '8 years',
+];
+
+const LANGUAGES = [
+  'Mandarin',
+  'Spanish',
+  'English',
+  'Hindi',
+  'Portugese',
+  'Bengali',
+  'Russian',
+  'Japanese',
+  'Vietnamese',
+  'Turkish',
+];
+
+export const APPLICATIONS = new Array(50).fill(null).map(() => ({
+  name: faker.person.fullName(),
+  university: UNIVERSITIES[findRandomEntry(1, UNIVERSITIES.length)],
+  country: faker.location.country(),
+  duration: DURATIONS[findRandomEntry(1, DURATIONS.length)],
+  cost: faker.number.int({ min: 1000, max: 300000 }),
+  applicationDeadlineDate: faker.date.between({
+    from: '2024-01-01T00:00:00.000Z',
+    to: '2030-01-01T00:00:00.000Z',
+  }),
+  language: LANGUAGES[findRandomEntry(1, LANGUAGES.length)],
+}));
