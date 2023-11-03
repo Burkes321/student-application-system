5b692164cab0e5a1065675a97cb95a9893b2a765
Sam Burke <sam.burke99@gmail.com>
2023-11-04 00:01:42 +0100

    implement table component

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,8 +1,56 @@
+import { Table, Pagination } from '@mantine/core';
+import { useState } from 'react';
+
 import { APPLICATIONS } from './const';
 import css from './home.module.css';
 
 import { Page } from '../../components/page/Page';
 
 export const Home = () => {
-  return <Page></Page>;
+  console.log('🚀 ~ file: Home.tsx:5 ~ APPLICATIONS:', APPLICATIONS);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // cur page 0 shows 0 to 9
+  // then 10 to 19
+  // then 20 to 29
+
+  const rows = APPLICATIONS.map((application) => (
+    <Table.Tr key={application.cost} className={css.tableRow}>
+      <Table.Td>{application.name}</Table.Td>
+      <Table.Td>{application.university}</Table.Td>
+      <Table.Td>{application.country}</Table.Td>
+      <Table.Td>{application.duration}</Table.Td>
+      <Table.Td>{application.cost}</Table.Td>
+      <Table.Td>{application.applicationDeadlineDate.toDateString()}</Table.Td>
+      <Table.Td>{application.language}</Table.Td>
+    </Table.Tr>
+  )).slice((currentPage - 1) * 10, currentPage * 10);
+
+  // TODO: create a hook here which manages which list items to show
+
+  return (
+    <Page>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>University</Table.Th>
+            <Table.Th>Country</Table.Th>
+            <Table.Th>Duration</Table.Th>
+            <Table.Th>Cost</Table.Th>
+            <Table.Th>ApplicationDeadlineDate</Table.Th>
+            <Table.Th>Language</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+
+      <Pagination
+        className={css.pagination}
+        value={currentPage}
+        onChange={setCurrentPage}
+        total={APPLICATIONS.length / 10}
+      />
+    </Page>
+  );
 };
