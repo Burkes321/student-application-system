ebced2959fa1fb8b2e79792420f61f78317b5b2b
Sam Burke <sam.burke99@gmail.com>
2023-11-04 01:07:26 +0100

    rework random item generation, implement filters except for cost, create filter context file, find filter values dynamically

---
+++ src/routes/home/interfaces.ts
@@ -0,0 +1,9 @@
+export type Application = {
+  name: string;
+  university: string;
+  country: string;
+  duration: string;
+  cost: number;
+  applicationDeadlineDate: Date;
+  language: string;
+};
